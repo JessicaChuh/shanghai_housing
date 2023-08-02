f926355b636f4920e8aa9eb22cbd0fb901bc7017
Jessica Chuh <Jessica_Chuh@yahoo.com>
2023-08-02 16:27:01 +0800

    streamlit user interface

--- app.py
+++ app.py
@@ -20,17 +20,20 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+def user_house_info():
+    #collect these info to put into model
+    Size = st.number_input('How big is your apt?')
+    N_bed = st.number_input('How many bedrooms?')
+    N_bath = st.number_input('How many bathrooms?')
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    if st.checkbox('Floor heating'):
+        st.subheader('Raw data')
+        st.write(data)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+if st.button('Check market price'):
+    # print is visible in the server output, not in the page
+    print('button clicked!')
+    st.write('waiting for model to excute')
+else:
+    st.write('There can be different on the real price due to other conditions')
