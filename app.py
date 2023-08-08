50d2b56d87df2b552c245bff1c3df588931314ae
Jessica Chuh <Jessica_Chuh@yahoo.com>
2023-08-08 21:16:25 +0800

    feature engineering

--- app.py
+++ app.py
@@ -4,31 +4,24 @@ import numpy as np
 
 st.title("Shanghai housing")
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
 
 def user_house_info():
     #collect these info to put into model
-    Size = st.number_input('How big is your apt?')
-    N_bed = st.number_input('How many bedrooms?')
-    N_bath = st.number_input('How many bathrooms?')
+    Size = st.number_input('How big is your apt?', min_value=10, step=1)
+    N_bed = st.number_input('How many bedrooms?',min_value=1, step=1)
+    N_bath = st.number_input('How many bathrooms?',min_value=1, step=1)
 
-    if st.checkbox('Floor heating'):
+    if st.multiselect(
+        'What amenities does your apt come with?',
+        ['Floor Heating',
+         'Pool',
+         'Fitness Centers',
+         ]
+    ):
+
         st.subheader('Raw data')
-        st.write(data)
+        st.write()
 
 
 if st.button('Check market price'):
@@ -37,3 +30,5 @@ if st.button('Check market price'):
     st.write('waiting for model to excute')
 else:
     st.write('There can be different on the real price due to other conditions')
+
+Real_price = st.number_input('How much are you paying for your apt?', min_value=3000, step=50)
