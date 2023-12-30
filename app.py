46ee7501777f5e02746cce3234f4613a4090a256
Jessica Chuh <Jessica_Chuh@yahoo.com>
2023-12-30 23:51:22 +0800

    easy version app

--- app.py
+++ app.py
@@ -1,34 +1,65 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 
-st.title("Shanghai housing")
+model = joblib.load('random_forest_model.joblib')
+metro_encoder = joblib.load('metro_encoder.joblib')
+district_encoder = joblib.load('district_encoder.joblib')
 
+def main():
+    # User inputs
+    district = st.text_input("District", "")
+    metro = st.text_input("Metro", "")
+    balcony = st.checkbox("Balcony")
+    oven = st.checkbox("Oven")
+    recently_renovated = st.checkbox("Recently renovated")
+    air_filter = st.checkbox("Air Filter")
+    fitness_centers = st.checkbox("Fitness Centers")
+    floor_heating = st.checkbox("Floor Heating")
+    garden = st.checkbox("Garden")
+    historic_building = st.checkbox("Historic Building")
+    large_storage_room = st.checkbox("Large Storage Room")
+    parking = st.checkbox("Parking")
+    playground = st.checkbox("Playground")
+    pool = st.checkbox("Pool")
+    tennis_courts = st.checkbox("Tennis Courts")
+    wall_heating = st.checkbox("Wall heating")
+    water_filter = st.checkbox("Water Filter")
+    pets_allowed = st.checkbox("Pets Allowed")
 
+    # Process user input using the encoders
+    district_encoded = district_encoder.transform([[district]])
+    metro_encoded = metro_encoder.transform([[metro]])
 
-def user_house_info():
-    #collect these info to put into model
-    Size = st.number_input('How big is your apt?', min_value=10, step=1)
-    N_bed = st.number_input('How many bedrooms?',min_value=1, step=1)
-    N_bath = st.number_input('How many bathrooms?',min_value=1, step=1)
+    # Create a DataFrame with the processed input
+    input_data = pd.DataFrame(data=district_encoded, columns=district_encoder.get_feature_names_out(['District']))
+    input_data = pd.concat([input_data, pd.DataFrame(data=metro_encoded, columns=metro_encoder.get_feature_names_out(['Metro']))], axis=1)
 
-    if st.multiselect(
-        'What amenities does your apt come with?',
-        ['Floor Heating',
-         'Pool',
-         'Fitness Centers',
-         ]
-    ):
+    # Add additional user inputs to the DataFrame
+    input_data['Balcony'] = int(balcony)
+    input_data['Oven'] = int(oven)
+    input_data['Recently_renovated'] = int(recently_renovated)
+    input_data['Air_Filter'] = int(air_filter)
+    input_data['Fitness_Centers'] = int(fitness_centers)
+    input_data['Floor_Heating'] = int(floor_heating)
+    input_data['Garden'] = int(garden)
+    input_data['Historic_Building'] = int(historic_building)
+    input_data['Large_Storage_Room'] = int(large_storage_room)
+    input_data['Parking'] = int(parking)
+    input_data['Playground'] = int(playground)
+    input_data['Pool'] = int(pool)
+    input_data['Tennis_Courts'] = int(tennis_courts)
+    input_data['Wall_Heating'] = int(wall_heating)
+    input_data['Water_Filter'] = int(water_filter)
+    input_data['Pets_Allowed'] = int(pets_allowed)
 
-        st.subheader('Raw data')
-        st.write()
+    # Make the prediction
+    prediction = model.predict(input_data)
 
+    # Display the prediction
+    st.write("Predicted House Price:")
+    st.write(prediction)
 
-if st.button('Check market price'):
-    # print is visible in the server output, not in the page
-    print('button clicked!')
-    st.write('waiting for model to excute')
-else:
-    st.write('There can be different on the real price due to other conditions')
-
-Real_price = st.number_input('How much are you paying for your apt?', min_value=3000, step=50)
+if __name__ == '__main__':
+    main()
